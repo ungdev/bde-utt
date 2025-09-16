c7e8c19d5d51133471cfc76f1c46c95740b1ad70
Arthur Dodin <dodinarthur@gmail.com>
2025-09-16 08:48:04 +0200

    update(whitenoise): remove comments

--- bde/settings.py
+++ bde/settings.py
@@ -24,7 +24,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "whitenoise.runserver_nostatic",
     "mozilla_django_oidc",
     "members",
     "showcase",
@@ -34,7 +33,6 @@ MYPY_PLUGINS = ["mypy_django_plugin.main"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,7 +124,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/uploads/"
 MEDIA_ROOT = BASE_DIR / "uploads"
