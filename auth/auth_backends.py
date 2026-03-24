9493cca738a065dd610ba7873d064b35478b16eb
Arthur Dodin <dodinarthur@gmail.com>
2026-03-24 23:44:15 +0100

    feat: logging (can be disable)

--- auth/auth_backends.py
+++ auth/auth_backends.py
@@ -1,29 +1,66 @@
+import logging
+
 from mozilla_django_oidc.auth import (  # type: ignore[import-untyped]
     OIDCAuthenticationBackend,
 )
 
 
+logger = logging.getLogger(__name__)
+
+
 class CustomOIDCBackend(OIDCAuthenticationBackend):
+
+    @staticmethod
+    def _claims_context(claims):
+        return {
+            "sub": claims.get("sub"),
+            "preferred_username": claims.get("preferred_username"),
+            "email": claims.get("email"),
+            "name": claims.get("name"),
+        }
+
     def create_user(self, claims):
-        user = super().create_user(claims)
+        logger.debug("OIDC create_user start: %s", self._claims_context(claims))
+        try:
+            user = super().create_user(claims)
 
-        name = claims.get("name", user.username)
+            name = claims.get("name", user.username)
 
-        user.username = claims.get("preferred_username", user.username)
-        user.first_name = name.split(" ")[0] if " " in name else name
-        user.last_name = " ".join(name.split(" ")[1:]) if " " in name else ""
-        user.email = claims.get("email", "")
-        user.save()
+            user.username = claims.get("preferred_username", user.username)
+            user.first_name = name.split(" ")[0] if " " in name else name
+            user.last_name = " ".join(name.split(" ")[1:]) if " " in name else ""
+            user.email = claims.get("email", "")
+            user.save()
 
-        return user
+            logger.info("OIDC create_user success for username=%s", user.username)
+            return user
+        except Exception:
+            logger.exception(
+                "OIDC create_user failed: %s", self._claims_context(claims)
+            )
+            raise
 
     def update_user(self, user, claims):
-        name = claims.get("name", user.username)
+        logger.debug(
+            "OIDC update_user start for username=%s claims=%s",
+            user.username,
+            self._claims_context(claims),
+        )
+        try:
+            name = claims.get("name", user.username)
 
-        user.username = claims.get("preferred_username", user.username)
-        user.first_name = name.split(" ")[0] if " " in name else name
-        user.last_name = " ".join(name.split(" ")[1:]) if " " in name else ""
-        user.email = claims.get("email", "")
-        user.save()
+            user.username = claims.get("preferred_username", user.username)
+            user.first_name = name.split(" ")[0] if " " in name else name
+            user.last_name = " ".join(name.split(" ")[1:]) if " " in name else ""
+            user.email = claims.get("email", "")
+            user.save()
 
-        return user
+            logger.info("OIDC update_user success for username=%s", user.username)
+            return user
+        except Exception:
+            logger.exception(
+                "OIDC update_user failed for username=%s claims=%s",
+                user.username,
+                self._claims_context(claims),
+            )
+            raise
--- bde/env.py
+++ bde/env.py
@@ -115,3 +115,15 @@ class EnvConfig:
     @property
     def OIDC_RP_SCOPES(self) -> str:
         return self.get("OIDC_RP_SCOPES", "")
+
+    @property
+    def DJANGO_LOG_LEVEL(self) -> str:
+        return self.get("DJANGO_LOG_LEVEL", "INFO")
+
+    @property
+    def OIDC_LOG_LEVEL(self) -> str:
+        return self.get("OIDC_LOG_LEVEL", "DEBUG")
+
+    @property
+    def GUNICORN_LOG_LEVEL(self) -> str:
+        return self.get("GUNICORN_LOG_LEVEL", "debug")
